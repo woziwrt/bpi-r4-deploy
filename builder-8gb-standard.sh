64649cb18f7dc0a8ccc774e254200365aa233196
Petr Wozniak <petr.wozniak@gmail.com>
2026-04-21 05:47:10 +0200

    8gb builders refactor

--- builder-8gb-standard.sh
+++ builder-8gb-standard.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
-tar -xzf repo-cache/openwrt.tar.gz --strip-components=1 -C . --one-top-level=openwrt
-tar -xzf repo-cache/mtk-openwrt-feeds.tar.gz --strip-components=1 -C . --one-top-level=mtk-openwrt-feeds
+git clone --branch openwrt-25.12 https://github.com/openwrt/openwrt.git openwrt
+cd openwrt; git checkout 12e56ac8d4bc056768c962796f55531a6da2b4cf; cd -;		
+
+
+git clone --branch master https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds
+cd mtk-openwrt-feeds; git checkout 14dc256bd536382ea427712eb3896e669beeae71; cd -;	
 
 #\cp -r my_files/feed_revision mtk-openwrt-feeds/autobuild/unified/
 
@@ -54,14 +58,13 @@ chmod -R 755 feeds/packages/utils/modemdata/files/usr/share
 make defconfig
 
 mkdir -p staging_dir/target-aarch64_cortex-a53_musl/image/
-\cp ../my_files/bootloader-mt7988-ddr4/mt7988-spim-nand-ddr4-bl2.img staging_dir/target-aarch64_cortex-a53_musl/image/
-\cp ../my_files/bootloader-mt7988-ddr4/mt7988-spim-nand-ddr4-bl31.bin staging_dir/target-aarch64_cortex-a53_musl/image/
-\cp ../my_files/bootloader-mt7988-ddr4/mt7988-emmc-ddr4-bl2.img staging_dir/target-aarch64_cortex-a53_musl/image/
-\cp ../my_files/bootloader-mt7988-ddr4/mt7988-emmc-ddr4-bl31.bin staging_dir/target-aarch64_cortex-a53_musl/image/
-\cp ../my_files/bootloader-mt7988-ddr4/mt7988-sdmmc-ddr4-bl2.img staging_dir/target-aarch64_cortex-a53_musl/image/
-\cp ../my_files/bootloader-mt7988-ddr4/mt7988-sdmmc-ddr4-bl31.bin staging_dir/target-aarch64_cortex-a53_musl/image/
+\cp ../my_files/bootloader-mt7988-comb-8g/mt7988-spim-nand-comb-8g-bl2.img staging_dir/target-aarch64_cortex-a53_musl/image/
+\cp ../my_files/bootloader-mt7988-comb-8g/mt7988-spim-nand-comb-8g-bl31.bin staging_dir/target-aarch64_cortex-a53_musl/image/
+\cp ../my_files/bootloader-mt7988-comb-8g/mt7988-emmc-comb-8g-bl2.img staging_dir/target-aarch64_cortex-a53_musl/image/
+\cp ../my_files/bootloader-mt7988-comb-8g/mt7988-emmc-comb-8g-bl31.bin staging_dir/target-aarch64_cortex-a53_musl/image/
+\cp ../my_files/bootloader-mt7988-comb-8g/mt7988-sdmmc-comb-8g-bl2.img staging_dir/target-aarch64_cortex-a53_musl/image/
+\cp ../my_files/bootloader-mt7988-comb-8g/mt7988-sdmmc-comb-8g-bl31.bin staging_dir/target-aarch64_cortex-a53_musl/image/
 
 bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic build
 
-exit
 

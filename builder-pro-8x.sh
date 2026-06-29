70d73a0a3a3ff1fff7a2fde605e5ab482ed6de8e
lotos6680 <lotos6680@gmail.com>
2026-06-29 16:35:25 +0300

    Update builder-pro-8x.sh
    gemini

--- builder-pro-8x.sh
+++ builder-pro-8x.sh
@@ -31,6 +31,28 @@ git clone --branch git01 https://github.com/mediatek/mtk-openwrt-feeds mtk-openw
 cd openwrt
 bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic prepare
 
+# =====================================================================
+# ХАК ДЛЯ SFP МОДУЛЯ BAZIS 1G-T (Игнорирование сбойного PHY-probe)
+# =====================================================================
+mkdir -p target/linux/mediatek/patches-6.12
+cat << 'EOF' > target/linux/mediatek/patches-6.12/999-sfp-ignore-bazis-phy.patch
+--- a/drivers/net/phy/sfp.c
++++ b/drivers/net/phy/sfp.c
+@@ -2420,6 +2420,12 @@ static int sfp_sm_probe_phy(struct sfp *sfp, bool is_c45)
+ {
+ 	int err;
+ 
++	/* HACK: BAZIS SFP 1000BASE-T workaround */
++	if (strncasecmp(sfp->id.base.vendor_name, "BAZIS", 5) == 0) {
++		dev_info(sfp->dev, "BAZIS module detected, skipping buggy PHY probe.\n");
++		return -ENODEV;
++	}
++
+ 	if (!is_c45) {
+ 		err = sfp_sm_probe_for_phy(sfp, SFP_PHY_ADDR);
+ EOF
+# =====================================================================
+
 # platform.sh: register bpi-r4-pro-8x in fit_do_upgrade, fit_check_image, platform_copy_config
 python3 -c 'f="target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh"; c=open(f).read(); c=c.replace("\tbananapi,bpi-r4-lite|\\\n\tbazis,ax3000wm","\tbananapi,bpi-r4-lite|\\\n\tbananapi,bpi-r4-pro-8x|\\\n\tbazis,ax3000wm"); c=c.replace("\tbananapi,bpi-r4-lite|\\\n\tcmcc,rax3000m","\tbananapi,bpi-r4-lite|\\\n\tbananapi,bpi-r4-pro-8x|\\\n\tcmcc,rax3000m"); open(f,"w").write(c)'
 
@@ -48,71 +70,4 @@ rm -f target/linux/mediatek/patches-6.12/999-eth-06-mtk_eth_soc-support-ethernet
 # Remove upstream Frank-W DTS patch — we use Sinovoip-based DTS instead
 rm -f target/linux/mediatek/patches-6.12/046-v6.19-arm64-dts-mediatek-mt7988a-bpi-r4-pro-add-dts.patch
 \cp -r ../my_files/bpi-r4-pro/patches-kernel/* target/linux/mediatek/patches-6.12/
-\cp ../my_files/bpi-r4-pro/patches-uboot/471-add-bpi-r4-pro-8x.patch package/boot/uboot-mediatek/patches/
-#\cp ../my_files/bpi-r4-pro/patches-uboot/472-add-bpi-r4-pro-8x-makefile.patch package/boot/uboot-mediatek/patches/
-\cp ../my_files/bpi-r4-pro/uboot-mediatek-Makefile package/boot/uboot-mediatek/Makefile
-\cp ../my_files/bpi-r4-pro/arm-trusted-firmware-mediatek-Makefile package/boot/arm-trusted-firmware-mediatek/Makefile
-\cp -r ../my_files/w-sd-nand-mmc-nvme-ddr4-filogic.mk target/linux/mediatek/image/filogic.mk
-mv target/linux/mediatek/image/filogic-extra.mk target/linux/mediatek/image/filogic-extra.mk.disabled
-
-echo "CONFIG_BLK_DEV_NVME=y" >> target/linux/mediatek/filogic/config-6.12
-echo "CONFIG_TASK_IO_ACCOUNTING=y" >> target/linux/mediatek/filogic/config-6.12
-python3 -c 'content=open("package/kernel/linux/modules/netdevices.mk").read(); content=content.replace("  KCONFIG:=CONFIG_AS21XXX_PHY\n  FILES:= \\\n   $(LINUX_DIR)/drivers/net/phy/as21xxx.ko\n  AUTOLOAD:=$(call AutoLoad,18,as21xxx)", "  KCONFIG:=CONFIG_AS21XXX_PHY\n  FILES:= \\\n   $(LINUX_DIR)/drivers/net/phy/aeon_as21xxx.ko\n  AUTOLOAD:=$(call AutoLoad,18,aeon_as21xxx)"); open("package/kernel/linux/modules/netdevices.mk","w").write(content)'
-python3 -c 'content=open("target/linux/mediatek/filogic/config-6.12").read(); content=content.replace("CONFIG_AS21XXX_PHY=y", "CONFIG_AS21XXX_PHY=m"); open("target/linux/mediatek/filogic/config-6.12","w").write(content)'
-
-\cp -r ../my_files/999-fitblk-02-w-add-bpi-r4-nvme-fitblk.patch target/linux/mediatek/patches-6.12
-
-\cp -r ../my_files/sms-tool/ feeds/packages/utils/sms-tool
-\cp -r ../my_files/modemdata-main/ feeds/packages/utils/modemdata
-\cp -r ../my_files/luci-app-modemdata-main/luci-app-modemdata/ feeds/luci/applications
-\cp -r ../my_files/luci-app-lite-watchdog/ feeds/luci/applications
-\cp -r ../my_files/luci-app-sms-tool-js-main/luci-app-sms-tool-js/ feeds/luci/applications
-mkdir -p files/etc/uci-defaults
-\cp -r ../my_files/99-set-hostname files/etc/uci-defaults/
-chmod +x files/etc/uci-defaults/99-set-hostname
-\cp -r ../my_files/99-pro-8x-network files/etc/uci-defaults/
-chmod +x files/etc/uci-defaults/99-pro-8x-network
-
-mkdir -p files/etc
-\cp ../my_files/fw_env_pro8x_snand.config files/etc/fw_env.config
-
-
-mkdir -p files/root/install-dir
-\cp ../my_files/bpi-r4-install/install-nand-pro8x.sh files/root/install-dir/install-nand.sh
-chmod +x files/root/install-dir/install-nand.sh
-\cp ../my_files/bpi-r4-install/install-nvme-pro8x.sh files/root/install-dir/install-nvme.sh
-chmod +x files/root/install-dir/install-nvme.sh
-\cp ../my_files/bpi-r4-install/install-emmc-pro8x.sh files/root/install-dir/install-emmc.sh
-chmod +x files/root/install-dir/install-emmc.sh
-\cp ../my_files/bpi-r4-install/install-nvme-unifi.sh files/root/install-dir/install-nvme-unifi.sh
-chmod +x files/root/install-dir/install-nvme-unifi.sh
-mkdir -p files/usr/sbin
-\cp ../my_files/bpi-r4-install/boot-nvme files/usr/sbin/boot-nvme
-chmod +x files/usr/sbin/boot-nvme
-\cp ../my_files/bpi-r4-pro/files/usr/sbin/boot-nand files/usr/sbin/boot-nand
-chmod +x files/usr/sbin/boot-nand
-
-\cp -r ../my_files/luci-app-wifimgr/ package/luci-app-wifimgr/
-
-./scripts/feeds update -a
-./scripts/feeds install -a
-
-
-
-\cp ../my_files/fit.sh package/utils/fitblk/files/fit.sh
-
-\cp -r ../my_files/qmi.sh package/network/utils/uqmi/files/lib/netifd/proto/
-chmod -R 755 package/network/utils/uqmi/files/lib/netifd/proto
-chmod -R 755 feeds/luci/applications/luci-app-modemdata/root
-chmod -R 755 feeds/luci/applications/luci-app-sms-tool-js/root
-chmod -R 755 package/luci-app-wifimgr/root
-chmod -R 755 feeds/packages/utils/modemdata/files/usr/share
-
-\cp -r ../configs/my_defconfig-8x-full .config
-make defconfig
-echo "CONFIG_PACKAGE_trusted-firmware-a-mt7988-emmc-comb-4bg=y" >> .config
-echo "CONFIG_PACKAGE_trusted-firmware-a-mt7988-sdmmc-comb-4bg=y" >> .config
-echo "CONFIG_PACKAGE_trusted-firmware-a-mt7988-spim-nand-ubi-comb-4bg=y" >> .config
-
-
-bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic build
+\cp ../my_files/bpi
